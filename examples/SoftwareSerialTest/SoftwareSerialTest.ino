4009594ca1280b82f23bd40c4189cae249ed0955
Cooper Maa <coopermaa77@gmail.com>
2013-01-02 22:28:05 +0800

    Support SoftwareSerial Library and Arduino Leonardo
    Changes to support SoftwareSerial Library
    Support Arduino Leonardo (not tested)

---
+++ examples/SoftwareSerialTest/SoftwareSerialTest.ino
@@ -0,0 +1,60 @@
+/*
+  Receives Test Events from your phone.
+  After it gets a test message the led 13 will blink
+  for one second.
+*/
+ 
+#include <MeetAndroid.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+
+MeetAndroid meetAndroid(mySerial);
+int onboardLed = 13;
+
+void setup()  
+{
+  // In this example, we use Hardware Serial as a debug port
+  Serial.begin(9600);
+  Serial.println("SoftwareSerial Test Program");
+  
+  // use the baud rate your bluetooth module is configured to 
+  // not all baud rates are working well, i.e. ATMEGA168 works best with 57600
+  mySerial.begin(57600); 
+  
+  // register callback functions, which will be called when an associated event occurs.
+  // - the first parameter is the name of your function (see below)
+  // - match the second parameter ('A', 'B', 'a', etc...) with the flag on your Android application
+  meetAndroid.registerFunction(testEvent, 'A');  
+
+  pinMode(onboardLed, OUTPUT);
+  digitalWrite(onboardLed, HIGH);
+}
+
+void loop()
+{
+  meetAndroid.receive(); // you need to keep this in your loop() to receive events
+}
+
+/*
+ * This method is called constantly.
+ * note: flag is in this case 'A' and numOfValues is 0 (since test event doesn't send any data)
+ */
+void testEvent(byte flag, byte numOfValues)
+{
+  Serial.print("Received data:"); 
+  Serial.print(meetAndroid.getInt(), DEC);
+  Serial.println();
+  
+  flushLed(300);
+  flushLed(300);
+}
+
+void flushLed(int time)
+{
+  digitalWrite(onboardLed, LOW);
+  delay(time);
+  digitalWrite(onboardLed, HIGH);
+  delay(time);
+}
+
